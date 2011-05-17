b0339e411a43d9794c06f236b5b9e37c608d0f5a
Tijs Verkoyen <tijs@verkoyen.eu>
2011-05-17 21:14:57 +0200

    Bumped the version

--- dropbox.php
+++ dropbox.php
@@ -32,7 +32,7 @@
  * This software is provided by the author "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the author be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
  *
  * @author		Tijs Verkoyen <php-dropbox@verkoyen.eu>
- * @version		1.0.3
+ * @version		1.0.4
  *
  * @copyright	Copyright (c), Tijs Verkoyen. All rights reserved.
  * @license		BSD License
@@ -51,7 +51,7 @@ class Dropbox
 	const API_PORT = 443;
 
 	// current version
-	const VERSION = '1.0.3';
+	const VERSION = '1.0.4';
 
 
 	/**
