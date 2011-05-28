b0c92913b28b534db687c90f31813954e7a84537
Tijs Verkoyen <tijs@verkoyen.eu>
2011-05-28 10:44:15 +0200

    Added testfile

---
+++ tests/index.php
@@ -0,0 +1,42 @@
+<?php
+
+// require
+require_once 'config.php';
+require_once '../dropbox.php';
+
+// create instance
+$dropbox = new Dropbox(APPLICATION_KEY, APPLICATION_SECRET);
+$dropbox->setOAuthToken(TOKEN);
+$dropbox->setOAuthTokenSecret(TOKEN_SECRET);
+
+// oAuth dance
+//$response = $dropbox->oAuthRequestToken();
+//if(!isset($_GET['authorize'])) $dropbox->oAuthAuthorize($response['oauth_token'], 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] .'?authorize=true');
+//else $response = $dropbox->oAuthAccessToken($_GET['oauth_token']);
+//$response = $dropbox->token(EMAIL, PASSWORD);
+
+//$response = $dropbox->account(time() . '-dropbox@verkoyen.eu', PASSWORD, 'Tijs', 'Verkoyen');
+//$response = $dropbox->accountInfo();
+
+//$response = $dropbox->filesGet(self::BASE_PATH .'hàh@, $.txt');
+//$response = $dropbox->filesPost(self::BASE_PATH .'', realpath('../dropbox.php'));
+//$response = $dropbox->metadata(self::BASE_PATH .'');
+//$response = $dropbox->thumbnails(self::BASE_PATH .'image.png');
+
+//$response = $dropbox->fileopsCopy(self::BASE_PATH .'will_be_copied', self::BASE_PATH .'copy_'. time());
+//$response = $dropbox->fileopsCreateFolder(self::BASE_PATH .'created_'. time());
+//$response = $dropbox->fileopsDelete(self::BASE_PATH .'will_be_deleted');
+//$response = $dropbox->fileopsMove(self::BASE_PATH .'will_be_moved', self::BASE_PATH .'moved_'. time());
+
+// output (Spoon::dump())
+ob_start();
+var_dump($response);
+$output = ob_get_clean();
+
+// cleanup the output
+$output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+
+// print
+echo '<pre>' . htmlspecialchars($output, ENT_QUOTES, 'UTF-8') . '</pre>';
+
+?>
