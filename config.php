28a7831a340e5ecef57a119f76e6bf7bb5b06fa2
Andreas Fritsch <pawelf@web.de>
2011-07-18 21:04:39 +0200

    add lines to get the token variables

--- config.php
+++ config.php
@@ -5,7 +5,12 @@ $consumerKey = 'yourKey';
 $consumerSecret = 'yourSecret';
 $secretxorkey = 'foreyesonly';
 $dropbox = new Dropbox($consumerKey, $consumerSecret);
-//$token = $dropbox->token('Email', 'PW');
+/* Use this to generate the token
+$token = $dropbox->token('Email', 'PW');
+echo $token['token'].'<br>';
+echo $token['secret'];
+read the variables and fill in the next lines
+*/
 $tokendb = 'token';
 $tokendb_secret = 'token_secret'; 
 $dropbox->setOAuthToken($tokendb);
