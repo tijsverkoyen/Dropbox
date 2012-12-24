cbb6b4db97e6f9502058b3016c067c82bcb7ec28
Tijs Verkoyen <tijs@verkoyen.eu>
2012-12-24 18:51:22 +0100

    Code cleanup

--- tests/index.php
+++ tests/index.php
@@ -1,42 +1,42 @@
 <?php
 
-// require
+//require
+require_once '../../../autoload.php';
 require_once 'config.php';
-require_once '../dropbox.php';
+
+use \TijsVerkoyen\Dropbox\Dropbox;
 
 // create instance
 $dropbox = new Dropbox(APPLICATION_KEY, APPLICATION_SECRET);
-$dropbox->setOAuthToken(TOKEN);
-$dropbox->setOAuthTokenSecret(TOKEN_SECRET);
 
-// oAuth dance
+// The code below will do the oAuth-dance
 //$response = $dropbox->oAuthRequestToken();
 //if(!isset($_GET['authorize'])) $dropbox->oAuthAuthorize($response['oauth_token'], 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] .'?authorize=true');
 //else $response = $dropbox->oAuthAccessToken($_GET['oauth_token']);
 //$response = $dropbox->token(EMAIL, PASSWORD);
+//var_dump($response);
+//exit;
 
-//$response = $dropbox->account(time() . '-dropbox@verkoyen.eu', PASSWORD, 'Tijs', 'Verkoyen');
-//$response = $dropbox->accountInfo();
+$dropbox->setOAuthToken(TOKEN);
+$dropbox->setOAuthTokenSecret(TOKEN_SECRET);
 
-//$response = $dropbox->filesGet(BASE_PATH .'hàh@, $.txt');
-//$response = $dropbox->filesPost(BASE_PATH, realpath('../dropbox.php'));
-//$response = $dropbox->filesPost(BASE_PATH .'with spaces', realpath('../dropbox.php'));
-//$response = $dropbox->filesPost(BASE_PATH .'met spaties/', realpath('/Users/tijs/Projects/dropbox/tests/with spaces.txt'));
-//$response = $dropbox->metadata(BASE_PATH .'met spaties');
-//$response = $dropbox->thumbnails(BASE_PATH .'image.png');
+try {
+    $response = $dropbox->account(time() . '-dropbox@verkoyen.eu', PASSWORD, 'Tijs', 'Verkoyen');
+    $response = $dropbox->accountInfo();
+    $response = $dropbox->filesGet(BASE_PATH .'hàh@, $.txt');
+    $response = $dropbox->filesPost(BASE_PATH, realpath('../dropbox.php'));
+    $response = $dropbox->filesPost(BASE_PATH .'with spaces', realpath('../dropbox.php'));
+    $response = $dropbox->filesPost(BASE_PATH .'met spaties/', realpath('/Users/tijs/Projects/dropbox/tests/with spaces.txt'));
+    $response = $dropbox->metadata(BASE_PATH .'met spaties');
+    $response = $dropbox->thumbnails(BASE_PATH .'image.png');
 
-//$response = $dropbox->fileopsCopy(BASE_PATH . 'image.png', BASE_PATH . 'copy_' . time());
-//$response = $dropbox->fileopsCreateFolder(BASE_PATH .'created_'. time());
-//$response = $dropbox->fileopsDelete(BASE_PATH .'will_be_deleted');
-//$response = $dropbox->fileopsMove(BASE_PATH .'will_be_moved', BASE_PATH .'moved_'. time());
+    $response = $dropbox->fileopsCopy(BASE_PATH . 'image.png', BASE_PATH . 'copy_' . time());
+    $response = $dropbox->fileopsCreateFolder(BASE_PATH .'created_'. time());
+    $response = $dropbox->fileopsDelete(BASE_PATH .'will_be_deleted');
+    $response = $dropbox->fileopsMove(BASE_PATH .'will_be_moved', BASE_PATH .'moved_'. time());
+} catch (Exception $e) {
+    var_dump($e);
+}
 
-// output (Spoon::dump())
-ob_start();
+// output
 var_dump($response);
-$output = ob_get_clean();
-
-// cleanup the output
-$output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
-
-// print
-echo '<pre>' . htmlspecialchars($output, ENT_QUOTES, 'UTF-8') . '</pre>';
